a63cea34633299e0b03ca564091c9418f08d8285
Jamie Tiblis <jajamie@live.unc.edu>
2021-09-18 17:40:11 -0400

    Hello

--- app/src/main/java/com/example/covidfree/MainActivity.java
+++ app/src/main/java/com/example/covidfree/MainActivity.java
@@ -12,7 +12,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//hello 1
-}
-hello
-}
+//hello
