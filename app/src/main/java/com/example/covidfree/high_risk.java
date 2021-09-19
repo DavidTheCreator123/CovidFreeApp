60cf9e911f326642bfece3f1176d489bfef4786d
DavidTheCreator123 <salazar.david@ic.edu>
2021-09-18 23:53:47 -0400

    Debugging part 2

--- app/src/main/java/com/example/covidfree/high_risk.java
+++ app/src/main/java/com/example/covidfree/high_risk.java
@@ -9,6 +9,6 @@ public class high_risk extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_high_risk);
+        setContentView(R.layout.activity_rickFactor);
     }
 }
