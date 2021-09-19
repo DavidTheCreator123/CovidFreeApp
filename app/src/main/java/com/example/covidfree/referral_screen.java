d656170c45915184eb6e72648f5fabe1ab68ee47
Jamie Tiblis <jajamie@live.unc.edu>
2021-09-18 22:44:02 -0400

    Activity file updates

---
+++ app/src/main/java/com/example/covidfree/referral_screen.java
@@ -0,0 +1,14 @@
+package com.example.covidfree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class referral_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_referral_screen);
+    }
+}
