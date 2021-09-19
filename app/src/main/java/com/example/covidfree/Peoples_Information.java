efee8a97a1167eeb6a72393fa14285caf6cffb8f
Jamie Tiblis <jajamie@live.unc.edu>
2021-09-19 02:16:32 -0400

    Peoples_Information

---
+++ app/src/main/java/com/example/covidfree/Peoples_Information.java
@@ -0,0 +1,102 @@
+package com.example.covidfree;
+
+public class Peoples_Information {
+    int heartBPM;
+    int bloodPressure_Systolic;
+    int bloodPressure_Diastolic;
+    int oxygen_level;
+    int weight;
+    String immunocompromised;
+    int day;
+    int month;
+    int year;
+    int age;
+    int height;
+
+    public void setHeartBpm(int b) {
+        this.heartBPM = b;
+    }
+
+    public int getHeartBPM() {
+        return this.heartBPM;
+    }
+
+
+    public void setBloodPressure_Systolic(int a) {
+        this.bloodPressure_Systolic = a;
+    }
+
+    public int getBloodPressure_Systolic() {
+        return this.bloodPressure_Systolic;
+    }
+
+
+    public void setBloodPressure_Diastolic(int b) {
+        this.bloodPressure_Diastolic = b;
+    }
+
+    public int getBloodPressure_Diastolic() {
+        return this.bloodPressure_Diastolic;
+    }
+
+    public void setOxygen_level(int a) {
+        this.oxygen_level = a;
+    }
+
+    public int getOxygen_level() {
+        return this.oxygen_level;
+    }
+
+    public void setWeight(int b) {
+        this.weight = b;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public void setImmunocompromised(String a)
+    {
+        this.immunocompromised = a;
+    }
+
+    public String setImmunocompromised() {
+        return this.immunocompromised;
+    }
+
+    public void setDay(int b) {
+        this.day = b;
+    }
+     public int getDay()
+    {
+         return this.day;
+
+    }
+
+    public void setMonth(int a) {
+        this.month = a;
+    }
+    public int getMonth() {
+        return this.month;
+    }
+
+    public void setYear(int b) {
+        this.year = b;
+    }
+    public int getYear() {
+        return this.year;
+    }
+    public void setAge(int a) {
+        this.age = a;
+    }
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setHeight(int b) {
+        this.height = b;
+    }
+    public int getHeight() {
+        return this.height;
+    }
+}
