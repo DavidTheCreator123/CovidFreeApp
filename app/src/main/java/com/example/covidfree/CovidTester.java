2f8ed546671d185fb61a22a5cf08c4f6e9d44122
DavidTheCreator123 <salazar.david@ic.edu>
2021-09-19 01:28:16 -0400

    New Stuff before power nap

---
+++ app/src/main/java/com/example/covidfree/CovidTester.java
@@ -0,0 +1,43 @@
+package com.example.covidfree;
+
+public class CovidTester
+{
+    private void testBloodPressure(int systolic, int diastolic)
+    {
+
+    }
+
+    private void testOxygenLevel(int o2)
+    {
+
+    }
+
+    private void testHeartRate(int heartRate)
+    {
+        if(heartRate >= 100)
+            System.out.println("Irregular Heart Rate");
+        else
+            System.out.println("Normal Hear Rate");
+    }
+
+    private void testChronicLevel(String a)
+    {
+
+    }
+
+    private void testVaccine(String a)
+    {
+
+    }
+
+    private void testAge(int age)
+    {
+
+    }
+
+    private void testTemperature(int temp)
+    {
+
+    }
+
+}
--- app/src/main/java/com/example/covidfree/Peoples_Information.java
+++ app/src/main/java/com/example/covidfree/Peoples_Information.java
@@ -16,70 +16,54 @@ public class Peoples_Information {
     public void setHeartBpm(int b) {
         this.heartBPM = b;
     }
-
     public int getHeartBPM() {
         return this.heartBPM;
     }
-
-
     public void setBloodPressure_Systolic(int a) {
         this.bloodPressure_Systolic = a;
     }
-
     public int getBloodPressure_Systolic() {
         return this.bloodPressure_Systolic;
     }
-
-
     public void setBloodPressure_Diastolic(int b) {
         this.bloodPressure_Diastolic = b;
     }
-
     public int getBloodPressure_Diastolic() {
         return this.bloodPressure_Diastolic;
     }
-
     public void setOxygen_level(int a) {
         this.oxygen_level = a;
     }
-
     public int getOxygen_level() {
         return this.oxygen_level;
     }
-
     public void setWeight(int b) {
         this.weight = b;
     }
-
     public int getWeight() {
         return this.weight;
     }
-
     public void setImmunocompromised(String a)
     {
         this.immunocompromised = a;
     }
-
     public String setImmunocompromised() {
         return this.immunocompromised;
     }
-
     public void setDay(int b) {
         this.day = b;
     }
-     public int getDay()
+    public int getDay()
     {
          return this.day;
 
     }
-
     public void setMonth(int a) {
         this.month = a;
     }
     public int getMonth() {
         return this.month;
     }
-
     public void setYear(int b) {
         this.year = b;
     }
@@ -92,7 +76,6 @@ public class Peoples_Information {
     public int getAge() {
         return this.age;
     }
-
     public void setHeight(int b) {
         this.height = b;
     }
